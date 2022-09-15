2b10b8bcf18e7b0d7ee9e50ac92ee10668d023e7
Shakirs Franco <kirsshacofran@gmail.com>
2022-09-14 19:21:57 -0500

    HT3

---
+++ LAB1/lab1_3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 1.3</title>
+</head>
+<body>
+    <?php
+    
+    echo phpinfo();
+    ?>
+</body>
+</html>
