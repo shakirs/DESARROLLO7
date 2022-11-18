06a62abfad84a4d9532e18fd5efb8af1900a2ef5
Shakirs Franco <kirsshacofran@gmail.com>
2022-11-17 20:02:09 -0500

    SHAKIRS FRANCO VICTOR DELGADO ELIEZER IBARRA
    ILS232
    
    PROYECTO 2

--- Agenda_Digital_Api/api/producto/eliminar.php
+++ Agenda_Digital_Api/api/producto/eliminar.php
@@ -1,54 +1,42 @@
 <?
-//Api para eliminar tareas
-//encabezado requerido
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-//incluimos el archivo de la base de datos y los objetos
+//incluimos el archivo de configuracion de la base de datos
 include_once '../configuracion/conexion.php';
+//incluimos el archivo de objeto producto
 include_once '../objetos/tareas.php';
-
 //obtenemos la conexion a la base de datos
 $conexion = new Conexion();
 $db = $conexion->obtenerConexion();
-
 //instanciamos el objeto tareas
 $tarea = new tareas($db);
 
 //obtenemos los datos del producto a eliminar
 $data = json_decode(file_get_contents("php://input"));
 
-//verificamos que los datos no esten vacios
+//obtenemos el id del producto a eliminar verificando que no este vacio y procedemos a eliminarlo
 
-if(
-!empty($data->id)
-){
-
-    //asignamos los valores a los atributos del objeto
+if(!empty($data->id)){
     $tarea->id = $data->id;
-
-    //eliminamos el producto
-    if($tarea->eliminar()){
-        //codigo de respuesta
+    if($tarea->eliminar_tarea($data->id)){
+        //Código de respuesta
         http_response_code(200);
-        //mensaje de respuesta
+        //Mensaje de respuesta
         echo json_encode(array("mensaje" => "Tarea eliminada"));
     }else{
-        //codigo de respuesta
+        //Código de respuesta
         http_response_code(503);
-        //mensaje de respuesta
+        //Mensaje de respuesta
         echo json_encode(array("mensaje" => "No se pudo eliminar la tarea"));
     }
 }else{
-    //codigo de respuesta
+    //Código de respuesta
     http_response_code(400);
-    //mensaje de respuesta
+    //Mensaje de respuesta
     echo json_encode(array("mensaje" => "No se pudo eliminar la tarea. Datos incompletos"));
 }
-
-
 
 ?>
